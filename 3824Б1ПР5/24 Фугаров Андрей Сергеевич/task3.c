a30528b4f60c8c75d4421659f101d3129e396fb7
andrewgeek1 <andrewfugi64@gmail.com>
2024-12-05 10:38:55 +0300

    Update task3.c

--- 3824Б1ПР5/24 Фугаров Андрей Сергеевич/task3.c
+++ 3824Б1ПР5/24 Фугаров Андрей Сергеевич/task3.c
@@ -1,5 +1,11 @@
-// Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+#include <stdio.h>
+
+double max_of_two_numbers(double a, double b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    double num1 = 5.3, num2 = 4.7;
+    printf("Максимум из %.2f и %.2f равен %.2f\n", num1, num2, max_of_two_numbers(num1, num2));
+    return 0;
 }
